5c147e1ed60a066139cddad8bb0ed71f411d7ca6
Elaine <elainedoud@gmail.com>
2023-01-26 13:46:58 -0500

    Adjusted Nav Bar for presentation screen

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -6,15 +6,15 @@ function NavBar (){
         <div>
             <nav class="navbar navbar-expand bg-primary">
                 <div class= "container">
-                <ul class="navbar-nav" >
+                <ul class="navbar-nav">
                     <li class="nav-item">
-                        <Link to='/enroll' class="nav-link ps-5 pe-5 ms-5 text-white">Enroll</Link>
+                        <Link to='/enroll' class="nav-link text-white px-5 ms-5">Enroll</Link>
                     </li>
                     <li class="nav-item">
-                        <Link to='/login' class="nav-link ps-5 pe-5 text-white">Login</Link>
+                        <Link to='/login' class="nav-link text-white px-5 ms-5">Login</Link>
                     </li>
                     <li class="nav-item">
-                        <Link to='/checking' class="nav-link ps-5 pe-5 text-white">Checking</Link>
+                        <Link to='/checking' class="nav-link text-white px-5 ms-5">Checking</Link>
                     </li>
                 </ul>
                 </div>
