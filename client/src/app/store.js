d46ecf7eded3c2fef1a495ef898c52904bfce150
Elaine <elainedoud@gmail.com>
2023-09-05 15:52:48 -0400

    Updated import file for Login Slice within store

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
-import loginReducer from '../features/login/loginSlice';
+import loginReducer from '../features/loginSlice';
 import checkingReducer from '../features/checking/checkingSlice'
 
     export  const store = configureStore({
